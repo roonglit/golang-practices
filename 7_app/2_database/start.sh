66dee1c3cfd11df8efd20c7578a548d754aa73b5
Roonglit <roonglit@gmail.com>
2025-06-25 10:21:49 +0700

    construct master.key from environment variable

--- 7_app/2_database/start.sh
+++ 7_app/2_database/start.sh
@@ -2,6 +2,13 @@
 
 set -e
 
+# Create master.key file from environment variable
+if [ ! -z "$MASTER_KEY" ]; then
+  echo "Creating master.key file from environment variable"
+  echo "$MASTER_KEY" > /app/config/master.key
+  chmod 600 master.key
+fi
+
 echo "run db migration"
 goose --dir=db/migrate -allow-missing postgres $DB_URI up
 
